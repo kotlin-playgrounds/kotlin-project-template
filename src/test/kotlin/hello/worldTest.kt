a26b0078e5a5b992c541d255d0cd14fce721ee8f
Benjamin Gudehus <hastebrot@gmail.com>
2018-01-17 21:49:34 +0100

    (test) Add example JUnit 5 test file.

---
+++ src/test/kotlin/hello/worldTest.kt
@@ -0,0 +1,13 @@
+package hello
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class WorldTest {
+
+    @Test
+    fun sample() {
+        assertEquals(5, 2 + 3)
+    }
+
+}
