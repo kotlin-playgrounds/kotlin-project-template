9b345277bdbbef66b009a3252fbca0804fa05003
Benjamin Gudehus <hastebrot@gmail.com>
2018-01-17 21:38:17 +0100

    (chore) Update Gradle to 4.5-rc-1, and Kotlin to 1.2.20.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val kotlinVersion by project
 val junitJupiterVersion by project
 val junitPlatformVersion by project
+val myLibraryVersion by extra { "0.0.0" }
 
 plugins {
-    kotlin("jvm") version "1.2.0"
+    kotlin("jvm") version "1.2.20"
 }
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 repositories {
     jcenter()
