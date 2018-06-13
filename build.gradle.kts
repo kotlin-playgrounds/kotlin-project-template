bc82566ae991369d480520b838fa067b70a041d8
Benjamin Gudehus <hastebrot@gmail.com>
2018-06-14 01:07:57 +0200

    (chore) Upgrade to Gradle 4.8, Kotlin 1.2.50, and use JUnit 5 Kotlin dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,42 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion by project
-val junitJupiterVersion by project
-val junitPlatformVersion by project
+val kotlinVersion: String by project
 val myLibraryVersion by extra { "0.0.0" }
 
 plugins {
-    kotlin("jvm") version "1.2.20"
-//    id("org.junit.platform") version "1.0.2"
+    val kotlinVersion = "1.2.50"
+
+    kotlin("jvm") version kotlinVersion
 }
-
-buildscript {
-    repositories {
-        jcenter()
-    }
-
-    dependencies {
-//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.20")
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.2")
-    }
-}
-
-apply {
-//    plugin("org.jetbrains.kotlin.jvm")
-    plugin("org.junit.platform.gradle.plugin")
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 repositories {
     jcenter()
 }
 
 dependencies {
-    compile(kotlin("stdlib", "$kotlinVersion"))
-    compile(kotlin("stdlib-jdk7", "$kotlinVersion"))
-    compile(kotlin("stdlib-jdk8", "$kotlinVersion"))
-    compile(kotlin("reflect", "$kotlinVersion"))
+    compile(kotlin("stdlib", kotlinVersion))
+    compile(kotlin("stdlib-jdk7", kotlinVersion))
+    compile(kotlin("stdlib-jdk8", kotlinVersion))
+    compile(kotlin("reflect", kotlinVersion))
 }
 
 dependencies {
-    testCompile(kotlin("test", "$kotlinVersion"))
-    testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testCompile("org.junit.platform:junit-platform-runner:$junitPlatformVersion")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-    testRuntime("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
+    testCompile(kotlin("test", kotlinVersion))
+    testCompile(kotlin("test-junit5", kotlinVersion))
+}
+
+kotlin {
+    experimental.coroutines = Coroutines.ENABLE
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    "wrapper"(Wrapper::class) {
+        gradleVersion = "4.8"
+        distributionUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-all.zip"
+    }
 }
